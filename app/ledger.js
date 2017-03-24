faa29093f87687f393058ddf6f6707c3be3e81d7
Marshall T. Rose <mrose17@gmail.com>
2017-03-24 12:16:58 -0700

    add createWorker...
    and use ledger-client@0.9.5

--- app/ledger.js
+++ app/ledger.js
@@ -94,7 +94,8 @@ const clientOptions = {
   debugP: process.env.LEDGER_DEBUG,
   loggingP: process.env.LEDGER_LOGGING,
   verboseP: process.env.LEDGER_VERBOSE,
-  server: process.env.LEDGER_SERVER_URL
+  server: process.env.LEDGER_SERVER_URL,
+  createWorker: app.createWorker
 }
 
 var doneTimer
